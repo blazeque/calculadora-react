c20dd613a276584a03adba1a721474326f82f1ab
Blazeque <daniel.blazeque@gmail.com>
2021-03-03 17:36:42 +0000

    Start Calculadora

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Calculadora from './main/Calculator';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <>
+    <h1>Calculadora</h1>
+   <Calculadora />
+  </>
+ ,
+  document.getElementById('root')
+);
+reportWebVitals();
